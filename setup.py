ae880e63eb4f909f4261f4409c6beb7876a9dc15
nahom <nahom.d54@gmail.com>
2025-08-23 09:20:13 +0300

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="django-optimize-image",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["Pillow", "Django"],
