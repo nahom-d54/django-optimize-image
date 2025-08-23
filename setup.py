ffc88b7349de8c0a10076f70b353dba256290cd7
nahom <nahom.d54@gmail.com>
2025-08-23 09:14:09 +0300

    Add testing framework and workflows for Django image optimization
    - Implement GitHub Actions workflows for testing across multiple Python and Django versions.
    - Create a pre-commit configuration for code quality checks.
    - Establish development requirements for testing dependencies.
    - Remove outdated requirements file and update setup configuration for versioning.
    - Add initial test cases for image optimization middleware.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="django-optimize-image",
@@ -7,7 +10,7 @@ setup(
     include_package_data=True,
     install_requires=["Pillow", "Django"],
     description="A Django middleware to optimize image uploads by converting them to WebP.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Nahom D",
     author_email="nahom@nahom.eu.org",
